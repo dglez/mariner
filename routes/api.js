5917e267b5b0629682d00954bc5d98d31edf4aeb
Carlos Sanchez <CarlosSanchez@Carloss-MacBook-Pro-3.local>
2017-04-01 23:02:36 -0400

    Some cosmetic changes to the routes api files

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,14 @@
 /**
  * Created by DG on 4/1/17.
+ * Sensor types that are in use:
+ * 'temperature'
+ * 'battery'
+ * 'light'
  */
 
 var express = require('express');
 var db = require('../db/connection.js');
-var Event = require('../models/event.js');
-
-var Battery = require('../models/battery.js');
 var Sensor = require('../models/sensorData.js');
-
 var middleware = require("../middleware/middleware.js");
 
 var router = express.Router();
@@ -60,64 +60,64 @@ router.get('/sensor/:type/:days/', function(req, res, next) {
     });
 });
 
-/**
- * This mapping allows to search for an event given its id.
- */
-router.get('/events/:id', function(req, res, next) {
-
-    Battery.find({'_id' : req.params.id}, function(error, events){
-        if(!events){
-            res.send({message: "Event not found"});
-        }
-        res.send(events);
-    });
-});
-
-/**
- * This mapping allows to up vote and event given its id
- */
-router.post('/events/upvote',  function(req, res, next) {
-
-    //var clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-    Event.findOne({'_id' : req.body.id}, function(error, thisEvent){
-        if(!thisEvent){
-            return res.send({error: "No event found with matching id!"});
-        } else {
-
-            thisEvent.upVote +=1;
-            thisEvent.save(function(error, resp){
-                if(error){
-                    return res.send({error: "No errors", score: thisEvent.upVote});
-                }else{
-                    return res.send({error: null, score: thisEvent.upVote});
-                }
-            });
-        }
-    });
-});
-
-/**
- * This mapping allows to down vote and event given its id
- */
-router.post('/events/downvote', function(req, res, next) {
-
-    Event.findOne({'_id' : req.body.id}, function(error, thisEvent){
-        if(!thisEvent){
-            return res.send({error: "No event found with matching id!"});
-        } else {
-            thisEvent.downVote +=1;
-            thisEvent.save(function(error, resp){
-                if(error){
-                    return res.send({error: "No errors", score: thisEvent.downVote});
-                }else{
-                    return res.send({error: null, score: thisEvent.downVote});
-                }
-            });
-        }
-    });
-});
-
+// /**
+//  * This mapping allows to search for an event given its id.
+//  */
+// router.get('/events/:id', function(req, res, next) {
+//
+//     Battery.find({'_id' : req.params.id}, function(error, events){
+//         if(!events){
+//             res.send({message: "Event not found"});
+//         }
+//         res.send(events);
+//     });
+// });
+//
+// /**
+//  * This mapping allows to up vote and event given its id
+//  */
+// router.post('/events/upvote',  function(req, res, next) {
+//
+//     //var clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+//
+//     Event.findOne({'_id' : req.body.id}, function(error, thisEvent){
+//         if(!thisEvent){
+//             return res.send({error: "No event found with matching id!"});
+//         } else {
+//
+//             thisEvent.upVote +=1;
+//             thisEvent.save(function(error, resp){
+//                 if(error){
+//                     return res.send({error: "No errors", score: thisEvent.upVote});
+//                 }else{
+//                     return res.send({error: null, score: thisEvent.upVote});
+//                 }
+//             });
+//         }
+//     });
+// });
+//
+// /**
+//  * This mapping allows to down vote and event given its id
+//  */
+// router.post('/events/downvote', function(req, res, next) {
+//
+//     Event.findOne({'_id' : req.body.id}, function(error, thisEvent){
+//         if(!thisEvent){
+//             return res.send({error: "No event found with matching id!"});
+//         } else {
+//             thisEvent.downVote +=1;
+//             thisEvent.save(function(error, resp){
+//                 if(error){
+//                     return res.send({error: "No errors", score: thisEvent.downVote});
+//                 }else{
+//                     return res.send({error: null, score: thisEvent.downVote});
+//                 }
+//             });
+//         }
+//     });
+// });
+//
 
 
 module.exports = router;
